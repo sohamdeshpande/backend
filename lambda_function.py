1b437604cbe051f475ef3a157ddc8618802d9d28
deshpande.soham <deshpande.soham@gmail.com>
2022-05-15 16:06:51 +0530

    first commit with demo code

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+client = boto3.resource("dynamodb")
+table = client.Table("CounterTable")
+id = 1
+def lambda_handler(event, context):
+    
+    # TODO implement
+    response = table.get_item(
+		Key={
+			'id':id
+			}
+		)
+    print(response['Item'])
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
